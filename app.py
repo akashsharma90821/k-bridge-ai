e9fed6b0b5d4c1cbdb4af23c4711fa28479857c4
akashsharma90821 <akashsharma90821@gmail.com>
2026-03-19 18:31:58 +0530

    Fixed app.run position

--- app.py
+++ app.py
@@ -1,8 +1,4 @@
 import os
-
-port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)
-
 from flask import Flask, render_template, request, session
 
 app = Flask(__name__)
@@ -293,7 +289,5 @@ def clear_chat():
 
 
 if __name__ == "__main__":
-    import os
-
 port = int(os.environ.get("PORT", 5000))
-app.run(host="0.0.0.0", port=port)   
+app.run(host="0.0.0.0", port=port)
